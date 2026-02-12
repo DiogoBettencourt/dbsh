b09f6f1431c401fe21c657b2839b6d8f3130f6c7
DiogoBettencourt <162571308+DiogoBettencourt@users.noreply.github.com>
2026-02-12 19:22:47 +0000

    Main loop prototype

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+
+int main() {
+    bool end = false;
+
+    while (!end) {
+        std::string input;
+        std::cout << "dbsh> ";
+        std::getline(std::cin, input);
+    }
+    
+}
