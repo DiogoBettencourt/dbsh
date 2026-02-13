61e06a84448e4db121cc52c60bedd19ca1cce43b
DiogoBettencourt <162571308+DiogoBettencourt@users.noreply.github.com>
2026-02-13 01:31:23 +0000

    Command logic in main implemented

--- main.cpp
+++ main.cpp
@@ -5,10 +5,22 @@
 int main() {
     bool end = false;
 
+    std::string validCommands[] = {"exit", "help", "clear"};
+
     while (!end) {
         std::string input;
         std::cout << "dbsh> ";
         std::getline(std::cin, input);
-    }
-    
+        for (const std::string& command : validCommands) {
+            if (input == "exit") {
+                end = true;
+                break;
+            }else if (command == input) {
+                //run command
+            } else {
+                std::cout << "Invalid Command" << std::endl;
+                break;
+            }
+        }
+    }   
 }
